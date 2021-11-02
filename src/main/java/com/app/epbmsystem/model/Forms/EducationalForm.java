db30597a912f2770ab32eb98b77220a3fb352b4d
awais.hafeez <hafeezzawais@gmail.com>
2021-11-02 14:37:21 +0500

    initial

--- src/main/java/com/app/epbmsystem/model/Forms/EducationalForm.java
+++ src/main/java/com/app/epbmsystem/model/Forms/EducationalForm.java
@@ -1,4 +1,15 @@
 package com.app.epbmsystem.model.Forms;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class EducationalForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    @Column(nullable = false)
+    private String name;
 }
