fecadd34f5cdd8153cdeef5de04e99409ada7a84
awais.hafeez <hafeezzawais@gmail.com>
2021-11-29 10:02:41 +0500

    JWT request

--- src/main/java/com/app/epbmsystem/model/Entity/JwtRequest.java
+++ src/main/java/com/app/epbmsystem/model/Entity/JwtRequest.java
@@ -1,38 +1,38 @@
-//package com.app.epbmsystem.model.Entity;
-//
-//import java.io.Serializable;
-//
-//public class JwtRequest implements Serializable {
-//
-//    private static final long serialVersionUID = 5926468583005150707L;
-//
-//    private String firstName;
-//    private String password;
-//
+package com.app.epbmsystem.model.Entity;
+
+import java.io.Serializable;
+
+public class JwtRequest implements Serializable {
+
+    private static final long serialVersionUID = 5926468583005150707L;
+
+    private String userName;
+    private String password;
+
 //    need default constructor for JSON Parsing
-//    public JwtRequest()
-//    {
-//
-//    }
-//
-//    public JwtRequest(String firstName, String password) {
-//        this.setFirstName(firstName);
-//        this.setPassword(password);
-//    }
-//
-//    public String getFirstName() {
-//        return this.firstName;
-//    }
-//
-//    public void setFirstName(String username) {
-//        this.firstName = username;
-//    }
-//
-//    public String getPassword() {
-//        return this.password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
+    public JwtRequest()
+    {
+
+    }
+
+    public JwtRequest(String userName, String password) {
+        this.setUserName(userName);
+        this.setPassword(password);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
