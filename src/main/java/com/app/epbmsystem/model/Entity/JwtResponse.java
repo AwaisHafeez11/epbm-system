c4232b3d5adbf0a71b7d5b2a2b3e98040c7fef97
awais.hafeez <hafeezzawais@gmail.com>
2021-11-30 16:19:27 +0500

    JWT response

--- src/main/java/com/app/epbmsystem/model/Entity/JwtResponse.java
+++ src/main/java/com/app/epbmsystem/model/Entity/JwtResponse.java
@@ -1,17 +1,17 @@
-//package com.app.epbmsystem.model.Entity;
-//
-//import java.io.Serializable;
-//
-//public class JwtResponse implements Serializable {
-//
-//    private static final long serialVersionUID = -8091879091924046844L;
-//    private final String jwtToken;
-//
-//    public JwtResponse(String jwtToken) {
-//        this.jwtToken = jwtToken;
-//    }
-//
-//    public String getToken() {
-//        return this.jwtToken;
-//    }
-//}
+package com.app.epbmsystem.model.Entity;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwtToken;
+
+    public JwtResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getToken() {
+        return this.jwtToken;
+    }
+}
